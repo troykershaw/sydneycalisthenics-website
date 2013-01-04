02c75515ef15e1f1157af367af01f3bb1669ef39
troykershaw <hi@troykershaw.com>
2013-01-04 16:30:57 +1100

    Simple node server
    Very simple node server to serve the site while developing.

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var connect = require('connect');
 connect.createServer(
+<<<<<<< HEAD
     connect.static(__dirname)
+=======
+    connect.static(__dirname + '/site/')
+>>>>>>> Simple node server
 ).listen(8080);
