c5dd9d2a91f0e8239cda0962b2a4cd667ab780cd
troykershaw <hi@troykershaw.com>
2013-01-05 20:49:30 +1100

    Fixed git conflicts

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 var connect = require('connect');
 connect.createServer(
-<<<<<<< HEAD
-    connect.static(__dirname)
-=======
     connect.static(__dirname + '/site/')
->>>>>>> Simple node server
 ).listen(8080);
